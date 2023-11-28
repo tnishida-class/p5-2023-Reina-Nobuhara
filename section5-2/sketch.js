558360501bd26cce5c528815e23251e74eb0728b
Reina-Nobuhara <rcherry1022@gmail.com>
2023-11-28 10:52:44 +0900

    sec5-2 change

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -1,36 +1,19 @@
 // テキスト「関数を作る(1) 引数のある関数」
 // 練習問題：星を描く関数を改造して正N角形を描画する関数を作ってみよう
+
 function setup(){
-  createCanvas(300, 100);
-  background(200);
-  fill(0);
-  crossmark(10, 10, 90, 90);
-  ngmark(150, 50, 80);
-  star(250, 50, 40);
-}
+  createCanvas(120, 120);
+  background(255);
+  noStroke();
 
-function crossmark(x1, y1, x2, y2){
-  line(x1, y1, x2, y2);
-  line(x2, y1, x1, y2);
-}
-
-function ngmark(cx, cy, r){
-  push();
-  noFill();
-  strokeWeight(r * 0.1);
-  let d = sqrt(r * r / 8);
-  ellipse(cx, cy, r);
-  line(cx - d, cy - d, cx + d, cy + d);
-  pop();
-}
-
-function star(cx, cy, r){
-  beginShape();
-  for(var i = 0; i < 5; i++){
-    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
-    let x = cx + cos(theta) * r;
-    let y = cy + sin(theta) * r;
-    vertex(x,y);
-  }
-  endShape(CLOSE);
+  fill(255, 204, 0);
+  beginShape();//図形の書き始めを示す命令
+  for(let i = 0; i < 5; i++){
+    let theta = TWO_PI * i / 5- HALF_PI ;//角度{方向）をtheta(シータ)で指定する//TWO_PI＝πの2倍＝1周分//3時方向が0度であるため、12時方向を0度とするためにマイナス半π
+    let x = width/2 + cos(theta) * 60;//円上の角度（theta）のX座標
+    let y = height/2 + sin(theta) * 60;//〃のY座標
+     //円周上のある角度（theta)の座標（X，y）は、円の半径（r）、円の中心座標（width/2,height/2)を用いてx=r*cos(theta)+width/2,y=r*sin(theta)+height/2で表すことが出来る
+    vertex(x, y);//次につなぐ点を一つ増やす
+  }//キャンパスの中心から円をn（ここでは5）等分し，360*1/5ずつずらしてポイントした5点をつなげる
+  endShape(CLOSE);//点をつなぎ終わる
 }
--- section5-4/sketch.js
+++ section5-4/sketch.js
@@ -1,76 +1,15 @@
-// テキスト「配列を受け取る関数」
 function setup(){
-  createCanvas(400, 400);
-  background(240);
-
-  let scores = [];
-  for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
-  }
-  console.log(scores);
-  barchart(scores);
-}
-
-// テキスト「配列と繰り返し」
-function sum(arr){
-  let n = 0;
-  for(let i = 0; i < arr.length; i++){ n += arr[i]; }
-  return n;
-}
-
-function average(arr){
-  return sum(arr) / arr.length;
-}
-
-function largest(arr){
-  let n = 0;
-  for(let i = 0; i < arr.length; i++){
-    n = max(n, arr[i]);
-  }
-  return n;
-}
-
-function smallest(arr){
-  let n = 100;
-  for(let i = 0; i < arr.length; i++){
-    n = min(n, arr[i]);
-  }
-  return n;
-}
-
-// テキスト「配列を使った描画」棒グラフ
-function barchart(scores){
-  scaleY(10);
-  const maxscore = largest(scores);
-  const minscore = smallest(scores);
-  const averagescore = average(scores);
-
+  createCanvas(120, 120);
+  background(0, 51, 153);
   noStroke();
 
-  for(let i = 0; i < scores.length; i++){
-    const dx = width / scores.length;
-    const h = height * scores[i] / 100;
-
-    if(scores[i] == maxscore){ fill(255, 0, 0); }
-    else if(scores[i] == minscore){ fill(0, 0, 255); }
-    else{ fill(128); }
-
-    rect(i * dx + 2, height - h, dx - 4, h);
-    fill(0);
-    text(scores[i].toPrecision(3), i * dx, height - h);
+  fill(255, 204, 0);
+  beginShape();
+  for(let i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = width/2 + cos(theta) * 50;
+    let y = height/2 + sin(theta) * 50;
+    vertex(x, y);
   }
-
-  const ay = height - height * averagescore / 100;
-  fill(0);
-  text(averagescore.toPrecision(3), 0, ay);
-  stroke(0, 255, 0);
-  line(0, ay, width, ay);
-}
-
-function scaleY(n){
-  for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-}
-
-function scaleX(n){
-  for(let i = 0; i < n; i++){ line(width * i / n, 0, width * i / n, height); }
+  endShape(CLOSE);
 }
